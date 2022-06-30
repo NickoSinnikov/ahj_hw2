6bf01824d21bfdda2b44eacfd667895cde7ed3ca
Nicko Sinnikov <nickosinnikov@outlook.com>
2022-06-30 17:32:28 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/4tqsw0dpsy4bxe7h?svg=true)](https://ci.appveyor.com/project/NickoSinnikov/ahj-hw1)
+
+https://nickosinnikov.github.io/ahj_hw1/
---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+const boardCell = Array.from(document.querySelectorAll(".cell"));
+const countCell = boardCell.length;
+let previousActiveCell = null;
+console.log(typeof previousActiveCell);
+function game() {
+  if (document.querySelector(".active_cell")) {
+    document.querySelector(".active_cell").remove();
+  }
+  let activeCellIndex = Math.floor(Math.random() * countCell);
+
+  if (previousActiveCell === activeCellIndex) {
+    activeCellIndex = Math.floor(Math.random() * countCell);
+  }
+  const activeCell = boardCell[activeCellIndex];
+  previousActiveCell = activeCellIndex;
+
+  const imgGoblin = document.createElement("div");
+  imgGoblin.classList.add("active_cell");
+  activeCell.appendChild(imgGoblin);
+}
+const intervalImg = setInterval(game, 1700);
+
+intervalImg;
