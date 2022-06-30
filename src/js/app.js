b3448593f8753bed31a5fa021bf890fb280c770b
Nicko Sinnikov <nickosinnikov@outlook.com>
2022-06-30 17:38:16 +0300

    lint

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,10 @@
-const boardCell = Array.from(document.querySelectorAll(".cell"));
+const boardCell = Array.from(document.querySelectorAll('.cell'));
 const countCell = boardCell.length;
 let previousActiveCell = null;
 console.log(typeof previousActiveCell);
 function game() {
-  if (document.querySelector(".active_cell")) {
-    document.querySelector(".active_cell").remove();
+  if (document.querySelector('.active_cell')) {
+    document.querySelector('.active_cell').remove();
   }
   let activeCellIndex = Math.floor(Math.random() * countCell);
 
@@ -14,8 +14,8 @@ function game() {
   const activeCell = boardCell[activeCellIndex];
   previousActiveCell = activeCellIndex;
 
-  const imgGoblin = document.createElement("div");
-  imgGoblin.classList.add("active_cell");
+  const imgGoblin = document.createElement('div');
+  imgGoblin.classList.add('active_cell');
   activeCell.appendChild(imgGoblin);
 }
 const intervalImg = setInterval(game, 1700);
